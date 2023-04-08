620d91c65956185a0e1994e5d39a168d0bb0cb88
ryanajacobpitogo <ryanjacob.pitogo.20@usjr.edu.ph>
2023-04-08 21:49:14 +0800

    initial provider with add and remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
@@ -5,19 +7,61 @@ import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        scaffoldBackgroundColor: const Color(0x00d52e54),
+    ChangeNotifierProvider(
+      create: (_) => TodoProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          scaffoldBackgroundColor: const Color(0x00d52e54),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const HomePage(),
+        routes: {
+          '/newTodo': (context) => const Material(),
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-      routes: {
-        '/newTodo': (context) => const Material(),
-      },
     ),
   );
+}
+
+
+class Todo {
+  final String id;
+  String label;
+  String desc;
+  bool isCompleted;
+
+  Todo({
+    required this.label,
+    required this.desc,
+    required this.isCompleted,
+  }) : id = const Uuid().v4();
+
+  void checked() {
+    isCompleted = true;
+  }
+
+  @override
+  bool operator ==(covariant Todo other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+class TodoProvider extends ChangeNotifier {
+  final List<Todo> _todoList = [];
+  UnmodifiableListView<Todo> get todoList => UnmodifiableListView(_todoList);
+
+  void add (Todo todo){
+    _todoList.add(todo);
+    notifyListeners();
+  }
+
+  void remove (String id) {
+    _todoList.removeWhere((e) => e.id == id);
+    notifyListeners();
+  }
 }
 
 //First Screen
